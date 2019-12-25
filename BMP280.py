5bfa0313918f8fbbfd4f306c49335acd68505ac5
Antsci <38735415+Antsci@users.noreply.github.com>
2019-12-25 23:18:47 +0000

    Update BMP280.py

--- BMP280.py
+++ BMP280.py
@@ -1,12 +1,13 @@
-'''
-provides pressure(hPa) and backups for temperature(C) and altitude(m)
-'''
+
 import time
 import board
 import busio
 import adafruit_bmp280
 
 class pressure_sensor:
+'''
+provides pressure(hPa) and backups for temperature(C) and altitude(m)
+'''
     i2c = busio.I2C(board.SCL, board.SDA)
     bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
     ground_pressure = bmp280.sea_level_pressure 
