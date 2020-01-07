6668ec1591fa1d916b225e8722715defafb0bf32
JacobInwald <42354019+JacobInwald@users.noreply.github.com>
2020-01-07 17:10:38 +0000

    Update BMP280.py
    Just added a tick method, and made sure there was continuous data retrieval.

--- BMP280.py
+++ BMP280.py
@@ -5,23 +5,36 @@ import busio
 import adafruit_bmp280
 
 class pressure_sensor:
+    
 '''
 provides pressure(hPa) and backups for temperature(C) and altitude(m)
 '''
-    i2c = busio.I2C(board.SCL, board.SDA)
-    bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
-    ground_pressure = bmp280.sea_level_pressure 
-
     def __init__(self, ground_pressure):
-        self.ground_pressure = ground_pressure
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+        '''
+            creating ground readings of basic data for later comparison
+        '''
+        self.pressureList = []
+        self.altitudeList = []
+        self.temperatureList = []
+        self.pressureList.append(self.getPressure)
+        self.altitudeList.append(self.getAltitude)
+        self.temperatureList.append(self.getTemperature)
 
-    def pressure(self):
-        return self.pmp280.pressure
+    def getPressure(self):
+        return self.bmp280.pressure
 
-    def temperature(self):
+    def getTemperature(self):
         return self.bmp280.temperature
 
-    def altitude(self):
+    def getAltitude(self):
         return self.bmp280.altitude
     
- 
+    def tick(self):
+        '''
+            to be run continuously to get updates throughout flight
+        '''
+        self.pressureList.append(self.get_pressure())
+        self.altitudeList.append(self.get_altitude())
+        self.temperatureList.append(self.get_append())
